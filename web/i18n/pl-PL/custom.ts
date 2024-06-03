54492d00c643d208ebc180403a587d4957de47a1
kelvintsim <lok-0929@hotmail.com>
2024-06-03 10:51:35 +0800

    Revert "Revert "modify title""
    This reverts commit c7d6cfdfd4f1fc9ab364f4ef1b49e7af31c8a542.

--- web/i18n/pl-PL/custom.ts
+++ web/i18n/pl-PL/custom.ts
@@ -1,31 +1,31 @@
 const translation = {
-  custom: 'Dostosowanie',
+  custom: "Dostosowanie",
   upgradeTip: {
-    prefix: 'Zaktualizuj swój plan, aby',
-    suffix: 'dostosować swoją markę.',
+    prefix: "Zaktualizuj swój plan, aby",
+    suffix: "dostosować swoją markę.",
   },
   webapp: {
-    title: 'Dostosuj markę aplikacji internetowej',
-    removeBrand: 'Usuń zasilane przez Dify',
-    changeLogo: 'Zmień obraz marki zasilany przez Brand',
-    changeLogoTip: 'Format SVG lub PNG o minimalnym rozmiarze 40x40px',
+    title: "Dostosuj markę aplikacji internetowej",
+    removeBrand: "Usuń zasilane przez Superbot",
+    changeLogo: "Zmień obraz marki zasilany przez Brand",
+    changeLogoTip: "Format SVG lub PNG o minimalnym rozmiarze 40x40px",
   },
   app: {
-    title: 'Dostosuj markę nagłówka aplikacji',
-    changeLogoTip: 'Format SVG lub PNG o minimalnym rozmiarze 80x80px',
+    title: "Dostosuj markę nagłówka aplikacji",
+    changeLogoTip: "Format SVG lub PNG o minimalnym rozmiarze 80x80px",
   },
-  upload: 'Prześlij',
-  uploading: 'Przesyłanie',
+  upload: "Prześlij",
+  uploading: "Przesyłanie",
   uploadedFail:
-    'Wystąpił problem podczas przesyłania obrazu, proszę spróbować ponownie.',
-  change: 'Zmień',
-  apply: 'Zastosuj',
-  restore: 'Przywróć domyślne',
+    "Wystąpił problem podczas przesyłania obrazu, proszę spróbować ponownie.",
+  change: "Zmień",
+  apply: "Zastosuj",
+  restore: "Przywróć domyślne",
   customize: {
-    contactUs: ' skontaktuj się z nami ',
-    prefix: 'Aby dostosować logo marki w aplikacji, proszę',
-    suffix: 'dla aktualizacji do wersji Enterprise.',
+    contactUs: " skontaktuj się z nami ",
+    prefix: "Aby dostosować logo marki w aplikacji, proszę",
+    suffix: "dla aktualizacji do wersji Enterprise.",
   },
-}
+};
 
-export default translation
+export default translation;
